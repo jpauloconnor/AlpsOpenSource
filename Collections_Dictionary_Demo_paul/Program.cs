a03a7b0b73edf0c9a7638f813848a62ea1fd5fa4
Paul <jamespauloconnor@gmail.com>
2016-05-06 22:51:17 -0400

    Added simple code for dictionary demo

---
+++ Collections_Dictionary_Demo_paul/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Dictionary_Demo_paul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("cat", "annoying creature");
+            dictionary.Add("dog", "man's best friend");
+
+            foreach (KeyValuePair<string, string> pair in dictionary)
+            {
+                Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
+            }
+            
+            Console.ReadLine();
+               
+        }
+    }
+}
