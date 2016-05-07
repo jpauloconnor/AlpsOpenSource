b11cbcde0da3df8afdb241f8fbf27aaf58bbea88
Paul <jamespauloconnor@gmail.com>
2016-05-06 20:31:10 -0400

    Added Constructor challenge answers

---
+++ AlpsOpenSource/Classes_ConstructorChallengeWithAnswer_paul/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_ConstructorChallengeWithAnswer_paul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paulsPhone = "I own the ";
+            MobilePhone myPhone = new MobilePhone("iPhone", "6");
+            Console.WriteLine(paulsPhone + myPhone.Brand + " " + myPhone.Version + ".");
+            Console.ReadLine();
+        }
+    }
+    public class MobilePhone
+    {
+        public MobilePhone(string brand, string version)
+        {
+            this.Brand = brand;
+            this.Version = version;
+        }
+        public string Brand { get; set; }
+        public string Version { get; set; }
+
+    }
+
+
+}
+
+
+
