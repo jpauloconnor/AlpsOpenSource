04ef14e9d07f799844c118a47ecb5a3beaf184f7
Paul <jamespauloconnor@gmail.com>
2016-05-07 23:10:50 -0400

    Added Interface Demo Part 2. Still needs a little work

---
+++ Interfaces_Demo_Part2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// See Part 1 for rules. This seciton will get into interface chaining. Talking about multiple interfaces inheriting from each other.
+/// Disclaimer: The methods are just dummy methods to show the relationships. These aren't necessarily real world examples. 
+/// 
+/// 1. Interfaces, like classes, can inherit from each other.
+/// 2. If an inteface inherits from another(IPerson2) 
+///     and the class(Person) inherits only from the inherited interface(IPerson2 and not IPerson1), 
+///     the class must implement the property(StateName) of the base interface(IPerson1). 
+/// </summary>
+namespace Interfaces_Demo_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Person person1 = new Person();
+            person1.StateName();
+        }
+    }
+
+    public class Person : IPerson2
+    {
+        public void StateName()
+        {
+            Console.WriteLine("My name is Jonas");
+        }
+    }
+
+    interface IPerson1
+    {
+        void StateName();
+    }
+    interface IPerson2 : IPerson1
+    {
+
+    }
+
+
+}
