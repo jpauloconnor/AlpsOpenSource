440d2676d821d39f3e850309c6ce4640f1f9e81d
Paul <jamespauloconnor@gmail.com>
2016-05-07 20:42:57 -0400

    Added a few shortcuts

--- KeyboardShortcuts_Chords/Program.cs
+++ KeyboardShortcuts_Chords/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace KeyboardShortcuts_Chords
 {
+    /// <summary>
+    /// Demo of three slashes for commenting. 
+    /// </summary>
+   
     class Program
     {
+        
         static void Main(string[] args)
         {
+            //CHORDS REQUIRE Holding Ctrl and then two keys in a row.
+            
+            //COMMON CHORDS 
+
+            //Comment & Uncomment
+            //Commment and ucomment the the following code:
+            Console.WriteLine("Ctrl + K -> Ctrl + C adds a comment");
+            Console.WriteLine("Ctrol + K -> Ctrl + U uncomments");
+
+            //XML Comment 
+            //Outside of this class and funtion 
+
+           
+
+            //REFACTORING
+            //Rename all instances -
+            Console.WriteLine("Ctrl R, R");
+
+            //CLick on cat -> Click Ctrl R,R -> Change to dog.
+            string cat = "Mabel";
+            Console.WriteLine(cat);
+            Console.WriteLine(cat.Length);
+
+
+
         }
+        
     }
 }
