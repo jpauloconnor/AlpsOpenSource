a497a3aae8897302cd02be705b8473c098f2a295
Paul <jamespauloconnor@gmail.com>
2016-05-07 22:53:44 -0400

    Added more rules to interfaces

--- Interfaces_Demo/Program.cs
+++ Interfaces_Demo/Program.cs
@@ -4,12 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Interface rules - Beginning
+/// 1. use interface keyword
+/// 2. Use I to name it. Common convention. Not an error.
+/// 3. Methods do not have an implementation. You can declare a method, but 
+/// you don't implement anything in it.
+/// Example: 
+///     Correct:
+///     void SayHello();
+///     Incorrect:
+///     void SayHello()
+///     {
+///         Console.WriteLine("Hello");
+///     }
+/// 4. Interface members are public by default. Can't use the public keyword or explicit
+/// access identifiers. Don't use "public" or "private".
+/// 5. Interfaces can't have fields.
+/// 6. Can be implemented by a class.
+/// 7. A class can inherit from an interface.
+/// 8. You must add the method into the class.
+/// </summary>
 namespace Interfaces_Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Person person1 = new Person();
+
         }
     }
+
+    interface IPerson
+    {
+        void SayHello();
+    }
+
+    public class Person : IPerson
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
 }
