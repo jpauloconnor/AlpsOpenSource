367e2c91bdb6b19440e65a36176ee325e9d07a15
Paul <jamespauloconnor@gmail.com>
2016-05-09 23:04:07 -0400

    Added JaggedArray code

---
+++ Collections_Multidimensional_JaggedArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Multidimensional_JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jagged array is an array of arrays
+            //Jagged because all inner arrays can be different sizes.
+        
+            string[][] jaggedArray = new string[3][]
+            {
+                new[] {"a","b"},
+                new[] {"a","b","c"},
+                new[] {"a", "b", "c", "d"}
+            };
+
+            Console.WriteLine(jaggedArray[1][2]);
+            Console.ReadLine();
+        }
+    }
+}
