de6e90e194202a0b8663e65da9c7a80772f7d3e0
Paul <jamespauloconnor@gmail.com>
2016-05-09 22:51:51 -0400

    Added method implementation to class

--- Interfaces_IEnumerable_Demo/Program.cs
+++ Interfaces_IEnumerable_Demo/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 /// <summary>
-/// IEnumerable is....
+/// IEnumerable is used to 
+/// 1. iterate through a collectino.
+/// 2. Can't modify(add/delete) data
+/// 3. Brings all data from server to client
 /// </summary>
 namespace Interfaces_IEnumerable_Demo
 {
@@ -13,6 +17,10 @@ namespace Interfaces_IEnumerable_Demo
     {
         static void Main(string[] args)
         {
+            IEnumerable <Employee = new List<Employee>
+            {
+
+            }
         }
     }
 }
