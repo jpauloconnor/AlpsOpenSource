63310d832a5cfb6c46034873934895b54ab6858e
Paul <jamespauloconnor@gmail.com>
2016-05-13 23:35:38 -0400

    Demo code for index.html. Gave explanation of bootstrap and html code. ->

---
+++ MVCDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCDemo
+{
+    public class RouteConfig
+    {
+        /// <summary>
+        /// This file fires up at App Start time and dictates how the requests go.
+        /// </summary>
+        /// <param name="routes"></param>
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            //Allows you to customize your URLs adn set default URLs.
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
