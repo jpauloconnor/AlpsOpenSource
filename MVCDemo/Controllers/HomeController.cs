aad2bdb3095f438419aedf5e143608c8b5e075e4
Paul <jamespauloconnor@gmail.com>
2016-05-16 22:38:36 -0400

    Added notes to the HomeController

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//Takes care of the logic for the Home page. 
+//These methods (Index, Browse and Details) within our Controller are called 
+//“Controller Actions”, and their job is to respond to URL requests 
+//and (generally speaking) determine what content should be sent back to the browser or user that invoked the URL.
 namespace MVCDemo.Controllers
 {
     public class HomeController : Controller
