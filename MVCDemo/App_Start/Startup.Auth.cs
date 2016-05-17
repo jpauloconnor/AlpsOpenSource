bff3cfeecf757547435f2ef0e3eacc43e6cd9638
Paul <jamespauloconnor@gmail.com>
2016-05-16 22:03:40 -0400

    Added more notes to Startup

--- MVCDemo/App_Start/Startup.Auth.cs
+++ MVCDemo/App_Start/Startup.Auth.cs
@@ -7,6 +7,15 @@ using Microsoft.Owin.Security.Google;
 using Owin;
 using MVCDemo.Models;
 
+
+/*
+    1. Configure db context
+    2. Enable cookies to be used to store info for signed in user. Use cookie to store info, configure cookie 
+    3. Validates security stamp at log in. Security feature for changing pw
+    4. Enables storing info during 2 step verification
+    5. Microsoft, Twitter, Google, Facebook Authentication
+     */
+
 namespace MVCDemo
 {
     public partial class Startup
@@ -14,11 +23,13 @@ namespace MVCDemo
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            //1
             // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
+            //2
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             // Configure the sign in cookie
@@ -28,6 +39,7 @@ namespace MVCDemo
                 LoginPath = new PathString("/Account/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
+                    //3
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
@@ -37,6 +49,7 @@ namespace MVCDemo
             });            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
+            //4
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
             app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
 
@@ -44,7 +57,8 @@ namespace MVCDemo
             // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
             // This is similar to the RememberMe option when you log in.
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-
+            
+            //5
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
             //    clientId: "",
